bdb81d716398e58cb39743ec5d72dbd55855f4a6
Daisy <w3442142262@163.com>
2026-03-24 23:33:47 +0800

    Initial commit: Init xinqing_map project structure

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="home-view">
+    <h1>心晴地图</h1>
+    <p>项目主页面开发中。</p>
+  </main>
+</template>
+
+<style scoped>
+.home-view {
+  min-height: 60vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+}
+</style>
